e56dd5d9677f9ca6760ad7b64d8797e95d58ef94
the miguel user <miguel@web.sdp.net.pe>
2019-06-15 00:59:45 -0500

    solr

---
+++ solr/solr.sh
@@ -0,0 +1,11 @@
+curl -X POST \
+  http://45.77.118.129:8983/solr/mycol1/dataimport \
+  -H 'Accept: application/json, text/javascript, */*; q=0.01' \
+  -H 'Accept-Encoding: gzip, deflate, br' \
+  -H 'Cache-Control: no-cache' \
+  -H 'Connection: keep-alive' \
+  -H 'Content-Type: application/x-www-form-urlencoded' \
+  -H 'Origin: http://localhost:8983' \
+  -H 'User-Agent: Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/68.0.3440.106 Safari/537.36' \
+  -H 'X-Requested-With: XMLHttpRequest' \
+  -d 'command=full-import&clean=true&commit=true&wt=json&indent=true&verbose=false&optimize=false&debug=false'
