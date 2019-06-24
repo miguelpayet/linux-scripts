3f58ff36bd97931fb466a732aa5dc020fe5876f6
miguel <miguelpayet@gmail.com>
2019-06-24 04:36:43 +0000

    solr

---
+++ solr/update-solr.sh
@@ -0,0 +1,2 @@
+curl -X POST   http://localhost:8983/solr/mycol1/dataimport?command=delta-import -H 'Accept: application/json, text/javascript, */*; q=0.01' -H 'Accept-Encoding: gzip, deflate, br' -H 'Accept-Language: en-US;q=0.8' -H 'Cache-Control: no-cache' -H 'Connection: keep-alive' -H 'Content-Type: application/x-www-form-urlencoded' -H 'Origin: http://localhost:8983' -H 'X-Requested-With: XMLHttpRequest'
+
